52f8cd347f47e78b55ffc121397a61c37ff3f48b
NNN1590 <nnn1590@nnn1590.org>
2020-04-23 02:25:52 +0900

    Improve sign.sh

--- sign.sh
+++ sign.sh
@@ -1,4 +1,5 @@
-#!bin/bash
+#!bin/bash -e
+cd "$(dirname ${0})"
 ionic cordova build android --prod --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore /Users/uparupa/majikiti/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
-zipalign -v 4 /Users/uparupa/majikiti/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk verion1.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
+zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk verion1.apk
