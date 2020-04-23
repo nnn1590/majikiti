edfed72d8f8267710eabfae3ba620512bae60d60
NNN1590 <nnn1590@nnn1590.org>
2020-04-23 16:51:02 +0900

    Fix android-release-build-and-sign.sh

--- android-release-build-and-sign.sh
+++ android-release-build-and-sign.sh
@@ -1,5 +1,5 @@
 #!bin/bash -e
-cd "$(dirname ${0})"
+cd "$(dirname "${0}")"
 ionic cordova build android --prod --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
 zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk verion1.apk
