89eea5e0b7406835854b707db4cc178f62420532
NNN1590 <nnn1590@nnn1590.org>
2020-05-02 16:43:44 +0900

    Fix typo

--- android-release-build-and-sign.sh
+++ android-release-build-and-sign.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "${0}")"
 ionic cordova build android --prod --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
-zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk verion1.apk
+zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk version1.apk
