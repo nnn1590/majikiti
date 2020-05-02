1f85539fb2f9c40839571b9e4c47f28c241506cd
NNN1590 <nnn1590@nnn1590.org>
2020-05-02 16:43:33 +0900

    Fix shebang

--- android-release-build-and-sign.sh
+++ android-release-build-and-sign.sh
@@ -1,4 +1,4 @@
-#!bin/bash -e
+#!/bin/bash -e
 cd "$(dirname "${0}")"
 ionic cordova build android --prod --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
